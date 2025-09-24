b6d35869e12a57ecb42ded54382214a081e42164
Kartikey <kartikeym004@gmail.com>
2025-09-25 00:29:07 +0530

    vercel fixes

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ console.log("Starting server process...");
 
 import express from "express";
 import cors from "cors";
-import routes from "../src/routes/index.js";
-import connectDB from "../src/config/db.js";
+import routes from "./src/routes/index.js";
+import connectDB from "./src/config/db.js";
 
 connectDB();
 const app = express();
@@ -14,12 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+  res.send("Backend is running 🚀");
 });
 
 app.use("/api", routes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
